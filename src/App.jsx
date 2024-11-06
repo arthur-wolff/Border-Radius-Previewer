5154cf8a33ad8df130aff56c45e6ddf7e28a26fe
Arthur <arthurwolffsoares@gmail.com>
2024-11-06 19:35:02 -0300

    0.2
    Estilo atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [rad1, setRad1] = useState(0)
   const [rad2, setRad2] = useState(0)
@@ -15,26 +16,32 @@ function App() {
     const radius2 = (document.getElementById('rad2').value)
     const radius3 = (document.getElementById('rad3').value)
     const radius4 = (document.getElementById('rad4').value)
+
     setRad1(radius1)
     setRad2(radius2)
     setRad3(radius3)
     setRad4(radius4)
 
-    console.log(rad1)
-    document.getElementById('box').style.borderRadius = `${rad1}px ${rad2}px ${rad3}px ${rad4}px` 
+    document.getElementById('box').style.borderRadius = `${rad1}px ${rad2}px ${rad4}px ${rad3}px`
+
   }
+
+  
 
   return (
     <div className='main'>
       <h1>Visualizador de Borda</h1>
+
       <div className='box' id='box'></div>
-      <input className='rad1' id='rad1' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
-      <input className='rad2' id='rad2' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
-      <input className='rad3' id='rad3' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
-      <input className='rad4' id='rad4' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
-      <button onClick={alterarBorda}>Alterar Borda</button>
-      <div>
-        
+      
+      <input className='rad' id='rad1' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
+      <input className='rad' id='rad2' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
+      <input className='rad' id='rad3' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
+      <input className='rad' id='rad4' type='text' maxLength={3} placeholder='00' defaultValue={0}/>
+      
+      <button onClick={alterarBorda} className='button'>Alterar Borda</button>
+      <div className='copyPaste'>
+       
       </div>
 
     </div>
